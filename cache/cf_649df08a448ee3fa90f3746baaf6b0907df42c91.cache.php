b7e614811f09df7361b4a75d175129e95daeda85
Nathaniel Wood <woodnathaniel@lcps.net>
2024-09-19 14:30:26 -0600

    a

---
+++ cache/cf_649df08a448ee3fa90f3746baaf6b0907df42c91.cache.php
@@ -0,0 +1,22 @@
+<?php if (!defined('IN_SCRIPT')) {die();} $hesk_settings['custom_fields']=array (
+  'custom1' => 
+  array (
+    'use' => '1',
+    'place' => '0',
+    'type' => 'text',
+    'req' => '1',
+    'category' => 
+    array (
+      0 => '1',
+    ),
+    'name' => 'Discord ID',
+    'value' => 
+    array (
+      'max_length' => 255,
+      'default_value' => '',
+    ),
+    'order' => '10',
+    'title' => 'Discord ID',
+    'name:' => 'Discord ID:',
+  ),
+);
